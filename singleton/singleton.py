66cd2d39a64aecc38726e7a9453073625fb9ebe7
bulat <bulatxajrutdinoff@yandex.ru>
2023-10-31 19:23:16 +0300

    Singleton

---
+++ singleton/singleton.py
@@ -0,0 +1,7 @@
+"""
+Motivation:
+  - for some components, it only makes sense to have one in the system (object factory)
+  - when initializer call is expensive
+
+Singleton - a component which is instantiated only once
+"""
